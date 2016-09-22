a83319c9b129a2793c8fb6a4738fe37750db9646
Sebastian Kreft <seba.kreft@gmail.com>
2016-09-22 22:14:48 +0200

    FDSJ28 Checkpoint commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 /**
  * Created by natvet on 14.09.16.
  */
+
+<!-- Start - Nawigacja-->
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    var navDspl = getComputedStyle(navLinks, null).display;
+    if (navDspl == "flex") {navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "flex";
+    }
+}
+
+// (function() {
+//     document.getElementById("nav-unfold-btn").setAttribute("onclick", "toggleMenu()");
+// })();
+
+<!-- End - Nawigacja-->
