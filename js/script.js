b4fd397a13bad55bfea363d4ca1700eee5aa470f
Sebastian Kreft <seba.kreft@gmail.com>
2016-09-23 11:18:22 +0200

    FDSJ-28 Checkpoint commit

--- js/script.js
+++ js/script.js
@@ -5,15 +5,20 @@
 <!-- Start - Nawigacja-->
 function toggleMenu() {
     var navLinks = document.getElementById("nav-links");
+    var navBtn = document.getElementById("nav-btn");
     var navDspl = getComputedStyle(navLinks, null).display;
-    if (navDspl == "flex") {navLinks.style.display = "none";
+
+    if (navDspl == "flex") {
+        navLinks.style.display = "none";
+        navBtn.style.backgroundImage = "url(../images/icon-white-menu.svg)"
     } else {
         navLinks.style.display = "flex";
+        navBtn.style.backgroundImage = "url(../images/icon-white-janusz.svg)"
     }
 }
 
-// (function() {
-//     document.getElementById("nav-unfold-btn").setAttribute("onclick", "toggleMenu()");
-// })();
+(function() {
+    document.getElementById("nav-btn").setAttribute("onclick", "toggleMenu()");
+})();
 
 <!-- End - Nawigacja-->
