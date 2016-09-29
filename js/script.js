3c8288beb7cd51ce8377c1269fbeafccf6b2c24b
Natalia Vetter <nataliavetter@gmail.com>
2016-09-29 16:55:22 +0200

    FDSJ-9 highlighting current section in nav-bar added

--- js/script.js
+++ js/script.js
@@ -3,25 +3,47 @@
  */
 
 <!-- Start - Nawigacja-->
-function toggleMenu() {
-    var navLinks = document.getElementById("nav-links");
-    var navButton = document.getElementById("nav-button");
-    var navDislplay = getComputedStyle(navLinks, null).display;
+$(document).ready(function () {
 
-    if (navDislplay == "flex") {
-        navLinks.style.display = "none";
-        navButton.style.backgroundImage = "url(images/icon-white-menu.svg)";
-    } else {
-        navLinks.style.display = "flex";
-        navButton.style.backgroundImage = "url(images/icon-white-menu-close.svg)";
-    }
-}
+    function toggleMenu() {
+        var navLinks = document.getElementById("nav-links");
+        var navButton = document.getElementById("nav-button");
+        var navDislplay = getComputedStyle(navLinks, null).display;
 
-(function() {
-    document.getElementById("nav-button").setAttribute("onclick", "toggleMenu()");
-    var w = window.innerWidth;
-    if (w <= 600) {
-        document.getElementById("nav-links").setAttribute("onclick", "toggleMenu()");
+        if (navDislplay == "flex") {
+            navLinks.style.display = "none";
+            navButton.style.backgroundImage = "url(images/icon-white-menu.svg)";
+        } else {
+            navLinks.style.display = "flex";
+            navButton.style.backgroundImage = "url(images/icon-white-menu-close.svg)";
+        }
     }
-})();
+
+    (function () {
+        document.getElementById("nav-button").setAttribute("onclick", "toggleMenu()");
+        var w = window.innerWidth;
+        if (w <= 600) {
+            document.getElementById("nav-links").setAttribute("onclick", "toggleMenu()");
+        }
+    })();
+
+
+    $(window).scroll(function () {
+        var position = $(this).scrollTop();
+
+        $('.nav-top > ul > li > a').removeClass('active');
+
+        $('section').each(function () {
+            var target = $(this).offset().top;
+            var id = $(this).attr('id');
+
+            if ($(this).hasClass('highlight-nav-top') && (position >= target)) {
+
+                $('.nav-top > ul > li > a').removeClass('active');
+                $('.nav-top > ul > li > a[href=#' + id + ']').addClass('active');
+            }
+        });
+    });
+
+});
 <!-- End - Nawigacja-->
