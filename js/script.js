991da3865771dd458672ea62949e97ba6d54118b
natvet <nataliavetter@gmail.com>
2016-10-02 18:05:54 +0200

    FDSJ-9 active link issues fixed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  */
 
 <!-- Start - Nawigacja-->
-$(document).ready(function () {
+
 
     function toggleMenu() {
         var navLinks = document.getElementById("nav-links");
@@ -27,20 +27,25 @@ $(document).ready(function () {
         }
     })();
 
+$(document).ready(function () {
+    var sections = $('section')
+        , nav = $('.nav-top')
+        , nav_height = nav.outerHeight();
 
-    $(window).scroll(function () {
-        var position = $(this).scrollTop();
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+        nav.find('a').removeClass('active');
+        sections.each(function() {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
 
-        $('.nav-top > ul > li > a').removeClass('active');
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                // nav.find('li').removeClass('active');
+                // sections.removeClass('active');
 
-        $('section').each(function () {
-            var target = $(this).offset().top;
-            var id = $(this).attr('id');
-
-            if ($(this).hasClass('highlight-nav-top') && (position >= target)) {
-
-                $('.nav-top > ul > li > a').removeClass('active');
-                $('.nav-top > ul > li > a[href=#' + id + ']').addClass('active');
+                // $(this).addClass('active');
+                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
             }
         });
     });
