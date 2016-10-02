4bc75d57257e1e3d322e12b9c174053531953791
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-02 15:39:57 +0200

    FDSJ-42 Added document ready event.

--- js/script.js
+++ js/script.js
@@ -2,26 +2,31 @@
  * Created by natvet on 14.09.16.
  */
 
-<!-- Start - Nawigacja-->
-function toggleMenu() {
-    var navLinks = document.getElementById("nav-links");
-    var navButton = document.getElementById("nav-button");
-    var navDislplay = getComputedStyle(navLinks, null).display;
+$(document).ready(function() {
 
-    if (navDislplay == "flex") {
-        navLinks.style.display = "none";
-        navButton.style.backgroundImage = "url(images/icon-white-menu.svg)";
-    } else {
-        navLinks.style.display = "flex";
-        navButton.style.backgroundImage = "url(images/icon-white-menu-close.svg)";
+    <!-- Start - Nawigacja-->
+    function toggleMenu() {
+        var navLinks = document.getElementById("nav-links");
+        var navButton = document.getElementById("nav-button");
+        var navDislplay = getComputedStyle(navLinks, null).display;
+
+        if (navDislplay == "flex") {
+            navLinks.style.display = "none";
+            navButton.style.backgroundImage = "url(images/icon-white-menu.svg)";
+        } else {
+            navLinks.style.display = "flex";
+            navButton.style.backgroundImage = "url(images/icon-white-menu-close.svg)";
+        }
     }
+
+    (function () {
+        document.getElementById("nav-button").setAttribute("onclick", "toggleMenu()");
+        var w = window.innerWidth;
+        if (w <= 600) {
+            document.getElementById("nav-links").setAttribute("onclick", "toggleMenu()");
+        }
+    })();
+    <!-- End - Nawigacja-->
+
+
 }
-
-(function() {
-    document.getElementById("nav-button").setAttribute("onclick", "toggleMenu()");
-    var w = window.innerWidth;
-    if (w <= 600) {
-        document.getElementById("nav-links").setAttribute("onclick", "toggleMenu()");
-    }
-})();
-<!-- End - Nawigacja-->
