473e926444f3c9c94b73393832708047eb0617e5
Marcin Andrzejewski <marcin@andrzejewski.me>
2016-10-03 16:30:25 +0200

    checkpoint

--- js/script.js
+++ js/script.js
@@ -25,3 +25,61 @@ function toggleMenu() {
     }
 })();
 <!-- End - Nawigacja-->
+
+// Start - mix-button
+
+function makeEmptyCell() {
+    var cell = $('td');
+    var clearCell = cell.empty();
+    return clearCell
+}
+
+function findEmptyCell() {
+    var emptyCell = $('td:empty').addClass('empty-cell');
+    return emptyCell;
+}
+
+function createRandomElement() {
+    var elements = [
+        'icon-black-mustache-v1',
+        'icon-black-mustache-v2',
+        'icon-black-mustache-v3',
+        'icon-blue-flipflops',
+        'icon-brown-bottle',
+        'icon-green-bottle',
+        'icon-green-onion',
+        'icon-grey-bag',
+        'icon-onion-brown',
+        'icon-purple-onion',
+        'icon-red-beetroot',
+        'icon-yellow-pint'
+    ];
+    var randomNumber = Math.floor(Math.random() * elements.length);
+    var createElement = function (src) {
+        img = new Image();
+        img.src = "images/game-icons/" + elements[randomNumber] + ".svg";
+        return img
+    };
+    return createElement();
+}
+function addRandomElementToEmptyCell() {
+   var emptyCell = findEmptyCell();
+    var addElement = emptyCell.map(function (cell) {
+        return cell.append(createRandomElement());
+
+    })
+   };
+    // var add = $emptyCell.prepend("test");
+    // return add;
+
+
+
+$('.game-mix-button').click(function () {
+    makeEmptyCell();
+    findEmptyCell();
+    createRandomElement();
+    addRandomElementToEmptyCell();
+});
+
+
+// End - mix-buton
